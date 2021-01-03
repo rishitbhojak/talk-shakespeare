882dfb5d318f473730734b9b175e0ea5c128482f
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-03 14:02:23 +0530

    Initial Commit
    Pushed to github

---
+++ app.js
@@ -0,0 +1,33 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json"
+
+function getTranslationURL(input)
+{
+   return serverURL + "?" +  "text=" + input;
+}
+
+function errorHandler(error) 
+{
+   console.log("error occured", error)
+}
+
+
+function clickHandler()
+{
+   var inputText= txtInput.value;   // taking inputs
+
+
+  // server processing call
+  fetch(getTranslationURL(inputText))
+  .then (response => response.json())
+  .then(json=>
+  {
+     var translatedText= json.contents.translated;
+     outputDiv.innerText = translatedText;
+  }
+  )
+  .catch(errorHandler)
+};
+btnTranslate.addEventListener("click", clickHandler)
